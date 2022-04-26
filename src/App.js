46cf1c8eb56da1534e9374646e1bb32a1b17f342
@WithTech-tsukada.sakiko <tsukada.sakiko@with-tech.co.jp>
2022-04-27 00:54:05 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,59 @@
+import React, {Component} from "react";
 
-function App() {
-  return  <div><h1>Hello World!</h1></div>
+// 属性の書き方例
+
+// 不要なタグを生まないためにReact.Fragmentを使う
+// class App extends Component {
+//   render(){
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+
+// for属性もJavaScriptの予約語とかぶってしまうので書き方が違う
+// Reactの制約でreturnで返すJSXは１つのタグでなければならないというものがあるのでdivで１つにしている
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </div>
+    )
+  }
 }
+
+// ■onChange
+// class App extends Component {
+//   render(){
+//     return <input type="text" onChange={() => {console.log("I am clicked.")}}/>
+//   }
+// }
+
+
+// ■inputでonClick
+// class App extends Component {
+//   render(){
+//     return <input type="text" onClick={() => {console.log("I am clicked.")}}/>
+//   }
+// }
+
+
+// JSXで変数やJavaScriptを使う
+// class App extends Component {
+//   render(){
+//     // 変数に格納した値を下記で使うというやり方もできる
+//     const greeting = "Hi, Tom!";
+
+//     // domという変数に入れて表示という書き方もできる
+//     const dom = <h1 className="foo">{greeting}</h1>
+//     return dom;
+//   }
+// }
 
 export default App;
