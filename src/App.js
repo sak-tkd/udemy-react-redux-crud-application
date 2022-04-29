66b0e10b52329a08186595e833b76c9b9ccbc0b7
@WithTech-tsukada.sakiko <tsukada.sakiko@with-tech.co.jp>
2022-04-30 02:57:32 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
-// これがクラスコンポーネント
-// class App extends Component {
-//   render(){
-//     return (
-//       <div>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => {console.log("I am clicked.")}} />
-//       </div>
-//     )
-//   }
-// }
-
 const App = () => {
+  // 属性を１つにまとめる
+  const profiles = [
+    { name: "Taro", age:10 },
+    { name: "Hanako", age:5 },
+    { name: "Noname" }
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          // キーを与えることでワーニングを回避
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   ) 
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old.</div>
+}
+
+// 設定がない場合のデフォルトの設定も可能
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
