41d51fa79254f3fe7a63e650e91656925ee030d3
@WithTech-tsukada.sakiko <tsukada.sakiko@with-tech.co.jp>
2022-05-02 20:21:58 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// ここで作られるstoreはアプリケーションで唯一のものになる
+const store = createStore(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
+registerServiceWorker();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
